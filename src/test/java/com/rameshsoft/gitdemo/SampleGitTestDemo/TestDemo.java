5299e95a44345dd913570886711d832c1496ddd6
srkrishna3384 <srkrishna3384@gmail.com>
2020-06-28 09:49:06 +0530

    changes done in testdemo

--- src/test/java/com/rameshsoft/gitdemo/SampleGitTestDemo/TestDemo.java
+++ src/test/java/com/rameshsoft/gitdemo/SampleGitTestDemo/TestDemo.java
@@ -3,6 +3,9 @@ package com.rameshsoft.gitdemo.SampleGitTestDemo;
 public class TestDemo {
 	public static void main(String[] args) {
 		System.out.println("Test Demo Class....");
+		System.out.println("Test Demo Class....");
+		System.out.println("Test Demo Class....");
+		
 	}
 
 }
