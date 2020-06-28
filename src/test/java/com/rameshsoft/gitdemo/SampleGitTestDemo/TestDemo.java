064709631d29ef30bc6fd53233f44dd5c55fdf30
srkrishna3384 <srkrishna3384@gmail.com>
2020-06-28 09:46:08 +0530

    added a new line

--- src/test/java/com/rameshsoft/gitdemo/SampleGitTestDemo/TestDemo.java
+++ src/test/java/com/rameshsoft/gitdemo/SampleGitTestDemo/TestDemo.java
@@ -3,6 +3,7 @@ package com.rameshsoft.gitdemo.SampleGitTestDemo;
 public class TestDemo {
 	public static void main(String[] args) {
 		System.out.println("Test Demo Class....");
+		System.out.println("Person one code");
 	}
 
 }
